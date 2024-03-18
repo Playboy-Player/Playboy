014beeca84f23899c562b796873c945c196ba359
yui <edkingkong@outlook.com>
2024-03-18 13:38:37 +0800

    update wbi helper

--- lib/backend/biliapi/wbi_helper.dart
+++ lib/backend/biliapi/wbi_helper.dart
@@ -7,14 +7,8 @@ import 'models/login_info_response.dart';
 import '../constants.dart';
 import 'package:crypto/crypto.dart';
 
+//https://github.com/SocialSisterYi/bilibili-API-collect/blob/master/docs/misc/sign/wbi.md
 class WbiHelper {
-  //if the format is broken by dart format, copy this below:
-  // static const mixinKeyEncTab = [
-  //   46, 47, 18, 2, 53, 8, 23, 32, 15, 50, 10, 31, 58, 3, 45, 35, 27, 43, 5, 49,
-  //   33, 9, 42, 19, 29, 28, 14, 39, 12, 38, 41, 13, 37, 48, 7, 16, 24, 55, 40,
-  //   61, 26, 17, 0, 1, 60, 51, 30, 4, 22, 25, 54, 21, 56, 59, 6, 63, 57, 62, 11,
-  //   36, 20, 34, 44, 52
-  // ];
   static const mixinKeyEncTab = [
     46,
     47,
@@ -82,8 +76,7 @@ class WbiHelper {
     52
   ];
   static Future<String> getKey() async {
-    //获取本地缓存
-    if (AppStorage().settings.wbiKey != "error" &&
+    if (AppStorage().settings.wbiKey != 'none' &&
         DateTime.fromMillisecondsSinceEpoch(AppStorage().settings.keyTime)
                 .day ==
             DateTime.now().day) {
@@ -93,11 +86,10 @@ class WbiHelper {
         await WebHelper().get("${Constants.apiBase}/x/web-interface/nav");
     LoginInfoResponse requestInfo = LoginInfoResponse.fromJson(request.data);
     if (requestInfo.data == null || requestInfo.data!.wbiImg == null) {
-      return "error";
+      return 'none';
     }
     var info = requestInfo.data!.wbiImg!;
-    if (info.subUrl == null || info.imgUrl == null) return "error";
-    //拼接并重排,取前32位
+    if (info.subUrl == null || info.imgUrl == null) return 'none';
     String raw = info.imgUrl!
             .substring(info.imgUrl!.lastIndexOf('/') + 1, info.imgUrl!.length)
             .split('.')[0] +
@@ -109,7 +101,6 @@ class WbiHelper {
       key += raw[x];
     }
     key = key.substring(0, 32);
-    //缓存新获取的值
     AppStorage().settings.wbiKey = key;
     AppStorage().settings.keyTime = DateTime.now().millisecondsSinceEpoch;
     AppStorage().saveSettings();
@@ -119,22 +110,19 @@ class WbiHelper {
   static Future<Map<String, dynamic>> signParams(
       Map<String, dynamic> params) async {
     String mixinKey = await getKey();
-    DateTime now = DateTime.now();
-    int currTime = (now.millisecondsSinceEpoch / 1000).round();
-    RegExp chrFilter = RegExp(r"[!\'\(\)*]");
-    List query = [];
-    Map newParams = Map.from(params)..addAll({"wts": currTime}); // 添加 wts 字段
-    // 按照 key 重排参数
-    List keys = newParams.keys.toList()..sort();
-    for (var i in keys) {
-      query.add(
-          '${Uri.encodeComponent(i)}=${Uri.encodeComponent(newParams[i].toString().replaceAll(chrFilter, ''))}');
+    // calculate wts
+    int wts = (DateTime.now().millisecondsSinceEpoch / 1000).round();
+    params.addAll({'wts': wts});
+    // calculate w_rid
+    RegExp filter = RegExp(r"[!\'\(\)*]");
+    List queries = [];
+    for (var key in params.keys.toList()..sort()) {
+      queries.add(
+          '${Uri.encodeComponent(key)}=${Uri.encodeComponent(params[key].toString().replaceAll(filter, ''))}');
     }
-    String queryStr = query.join('&');
-    String wbiSign =
-        md5.convert(utf8.encode(queryStr + mixinKey)).toString(); // 计算 w_rid
-    Map<String, dynamic> result = params
-      ..addAll({'wts': currTime.toString(), 'w_rid': wbiSign});
-    return result;
+    String wRid =
+        md5.convert(utf8.encode(queries.join('&') + mixinKey)).toString();
+    params.addAll({'w_rid': wRid});
+    return params;
   }
 }
